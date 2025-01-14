6092020732ee2ad9e31dbd98eff679e959040ee7
jitendra <jitendra@innowrap.com>
2025-01-14 14:58:06 +0530

    ""

--- src/components/SearchSection/index.tsx
+++ src/components/SearchSection/index.tsx
@@ -6,9 +6,9 @@ import Select from 'react-select'
 export default function SearchSection() {
   return (
     <div className="grid grid-cols-12 gap-2 md:gap-3">
-          <input type="text" id='searchbar_input' placeholder="Enter Keywords or a job title, e.g. 'sales & marketing'" className="col-span-12 md:col-span-6 min-w-[200px] xl:min-w-[480px] 2xl:min-w-[530px] px-6 py-2 xl:px-8 xl:py-4 rounded-full" />
+          <input type="text" id='searchbar_input' placeholder="Enter Keywords or a job title, e.g. 'sales & marketing'" className="placeholder:line-clamp-1 col-span-12 md:col-span-6 min-w-[200px] xl:min-w-[480px] 2xl:min-w-[530px] px-6 py-2 xl:px-8 xl:py-4 rounded-full" />
           <Select
-            value={null}
+            // value={null}
             options={[{value: 1, label:'Mumbai'}, {value: 2, label: 'Banglore'}, {value: 3, label: 'delhi'}, {value: 4, label: 'Hyderabad'}]}
             placeholder="City"
             className='col-span-12 md:col-span-2'
@@ -18,7 +18,7 @@ export default function SearchSection() {
             }}
           />
           <Select
-            value={null}
+            // value={null}
             options={[{value: 1, label:'Full Time'}, {value: 2, label: 'Part Time'}, {value: 3, label: 'Contract'}]}
             placeholder="Job Type"
             className='col-span-12 md:col-span-2'
